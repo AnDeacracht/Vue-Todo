5256f684cc3c5d06ba4d5dec2f90d0c0fc1f861b
AnDeacracht <dmaxmueller@gmail.com>
2018-03-18 16:49:05 +0100

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{ todos.filter(todo => todo.done === true).length }}</p>
+    <p>Pending Tasks: {{ todos.filter(todo => todo.done === false).length }}</p>
+    <todo v-for="(todo, index) in todos" v-bind:todo="todo" v-bind:key="index"></todo>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+import Todo from './Todo'
+
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  }
+}
+
+</script>
+
+<style type="text/css"></style>
